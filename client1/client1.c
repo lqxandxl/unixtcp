53bc207f1fc0cf7efd2303d6ecc71095c5ba25a5
liqixin <buptliqixin@163.com>
2018-11-15 15:18:03 +0800

    server and client basic tcp

--- client1/client1.c
+++ client1/client1.c
@@ -4,38 +4,49 @@ int main(int argc , char ** argv){
     int sockfd,n;
     char recvline[MAXLINE+1];
     struct sockaddr_in servaddr;
-
+    printf("argc is %d \n",argc); // ./client agrc is 1  ./client 127.0.0.1 agrc is 2
+    printf("MAXLINE is %d \n",MAXLINE);  //4096
     if(argc != 2){
-        return 0;
+        err_quit("agrc != 2");
     }
 
     sockfd = socket(AF_INET,SOCK_STREAM,0) ;
     if(sockfd<0){
-        return 0;
+        err_sys("socket < 0");
     }
 
     bzero(&servaddr,sizeof(servaddr)); //初始化
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(13);
+    servaddr.sin_port = htons(1992); //server 端口号
 
-    int tmp = inet_pton(AF_INET,argv[1],&servaddr.sin_addr);
+    int tmp = inet_pton(AF_INET,argv[1],&servaddr.sin_addr); //./client localhost 则报错，必须是127.0.0.1形式
     if(tmp<=0){
-        return 0;
+        err_quit("inet_pton error");
     }
 
     if(connect(sockfd,(SA *)&servaddr,sizeof(servaddr))<0){
-        return 0;
+        err_sys("connect error");
     }
-
-    while((n=read(sockfd,recvline,MAXLINE))>0){
+    memset(recvline,'0',sizeof(recvline)/sizeof(char)); //0初始化 就是空 '0'是字符
+    while((n=read(sockfd,recvline,MAXLINE))>0){ //不多加一个括号 n有问题
+        printf("%c\n",recvline[n-6]); //2
+        printf("%c\n",recvline[n-5]); //0
+        printf("%c\n",recvline[n-4]); //1
+        printf("n-3 %c\n",recvline[n-3]); // 8 26-3 = 23 
+        printf("n-2 %c\n",recvline[n-2]); // \r 24
+        printf("n-1 is %d  %c\n",n-1,recvline[n-1]); // \n 25
+        printf("n is %d  %c\n",n,recvline[n]); // not sure self char
         recvline[n]=0;
-        if(fputs(recvline,stdout) == EOF){
-            return 0;
+        int reclen = strlen(recvline);
+        printf("rec len is %d",reclen);
+        if(fputs(recvline,stdout) == EOF){ //int fputs(const char *str, FILE *stream); 需要包含最后一个字符 把str输出到stdout上面
+            err_sys("fput error");
         }
+        printf("in while n is %d \n",n);
     }
-
+    printf("out while n is %d \n",n);
     if(n<0){
-        return 0;
+        err_sys("n<0 read error");
     }
 
     exit(0);
---
+++ server1/server1.c
@@ -0,0 +1,39 @@
+#include "unp.h"
+// kill by ctrl+c need sudo netstat -apn | grep 1992
+
+int main(int argc , char ** argv){
+	int					listenfd, connfd;
+	struct sockaddr_in	servaddr;
+	char				buff[MAXLINE];
+	time_t				ticks;
+
+	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family      = AF_INET;  //填写套接字
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port        = htons(1992);	/* daytime server */
+
+	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr)); //Permission denied。这个是由于超0~1024的端口为公认端口，需要root权限运行。
+
+	Listen(listenfd, LISTENQ);
+
+	for ( ; ; ) {
+		connfd = Accept(listenfd, (SA *) NULL, NULL); //由listenfd产生一个新的connfd，在这个connfd上进行了操作
+
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks)); //字符放入buff中
+        Write(connfd, buff, strlen(buff)); //这里strlen自己加1，只会导致服务端多传了一个字符。
+        int bufflen = strlen(buff);
+        printf("bufflen is %d\n",bufflen);
+        char tmp[5];
+        tmp[0] = 'a';
+        tmp[1] = 'b';
+        tmp[2] = '\r';
+        tmp[3] = '\n';
+        tmp[4] = '\0';
+        int tmplen = strlen(tmp);
+        printf("tmplen is %d\n",tmplen);
+		Close(connfd); //关闭connfd
+	}
+}
